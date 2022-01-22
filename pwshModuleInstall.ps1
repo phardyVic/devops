069ca7d5349c25aa270e099dda992f2977e8859b
phardyVic <98220501+phardyVic@users.noreply.github.com>
2022-01-22 10:02:39 -0500

    Update pwshModuleInstall.ps1

--- pwshModuleInstall.ps1
+++ pwshModuleInstall.ps1
@@ -1,2 +1,9 @@
-Set-ExecutionPolicy -ExecutionPolicy Bypass
+Install-PackageProvider NuGet -Force
+
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+
+Install-Module PowerShellGet -Force
+
+Install-Module PSReadLine -Force
+
 Install-Module -Name Az -Repository PSGallery -Force
